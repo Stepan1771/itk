64e498b4a4cb027dcea30653c0fc416f0f738355
Stepan Karpov <stepan-karpov-2025@mail.ru>
2025-11-17 11:10:48 +0300

    create cache decorator

---
+++ 1_basic_python/cache_decorator.py
@@ -0,0 +1,77 @@
+import unittest.mock
+
+from collections import OrderedDict
+
+from functools import wraps
+
+
+def lru_cache(*args, **kwargs):
+    if len(args) == 1 and callable(args[0]) and not kwargs:
+        return _make_wrapper(args[0], None)
+
+    maxsize = kwargs.get('maxsize', None)
+
+    def decorator(func):
+        return _make_wrapper(func, maxsize)
+
+    return decorator
+
+
+def _make_wrapper(func, maxsize):
+    cache = OrderedDict()
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        key = (args, tuple(sorted(kwargs.items())))
+        if key in cache:
+            cache.move_to_end(key)
+            return cache[key]
+
+        result = func(*args, **kwargs)
+        cache[key] = result
+        cache.move_to_end(key)
+
+        if (maxsize is not None) and (len(cache) > int(maxsize)):
+            cache.popitem(last=False)
+
+        return result
+
+    return wrapper
+
+
+@lru_cache
+def sum(a: int, b: int) -> int:
+    return a + b
+
+
+@lru_cache
+def sum_many(a: int, b: int, *, c: int, d: int) -> int:
+    return a + b + c + d
+
+
+@lru_cache(maxsize=3)
+def multiply(a: int, b: int) -> int:
+    return a * b
+
+
+if __name__ == '__main__':
+    assert sum(1, 2) == 3
+    assert sum(3, 4) == 7
+
+    assert multiply(1, 2) == 2
+    assert multiply(3, 4) == 12
+
+    assert sum_many(1, 2, c=3, d=4) == 10
+
+    mocked_func = unittest.mock.Mock()
+    mocked_func.side_effect = [1, 2, 3, 4]
+
+    decorated = lru_cache(maxsize=2)(mocked_func)
+    assert decorated(1, 2) == 1
+    assert decorated(1, 2) == 1
+    assert decorated(3, 4) == 2
+    assert decorated(3, 4) == 2
+    assert decorated(5, 6) == 3
+    assert decorated(5, 6) == 3
+    assert decorated(1, 2) == 4
+    assert mocked_func.call_count == 4
